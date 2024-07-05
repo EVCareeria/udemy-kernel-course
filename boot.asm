a68050eff45c020ad5ab7d30a55c613372abfa3c
evcareeria <erik.vesa.contact@gmail.com>
2024-07-05 11:21:04 +0300

    [Printing text from a file]
    combination of Makefile and boot sector reading

--- boot.asm
+++ boot.asm
@@ -4,10 +4,12 @@ _start:
   jmp short start
   nop
 
- times 33 db 0
+times 33 db 0
 
 start:
     jmp 0x7c0:step2
+
+
 
 step2:
     cli ; Clear Interrupts
@@ -18,9 +20,25 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
-    mov si, message
+
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer 
+    int 0x13
+    jc error
+
+    mov si, buffer
     call print
+
     jmp $
+error:
+    mov si, error_message
+    call print
+    jmp  $
+
 
 print:
   mov bx, 0
@@ -38,6 +56,9 @@ print_char:
   int 0x10
   ret
 
-message: db 'Hello World!', 0
+error_message: db 'Failed to load the sector', 0
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
+    
