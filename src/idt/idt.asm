a862f5768c6fa07f573c1d4507734a12f39af1e5
evcareeria <erik.vesa.contact@gmail.com>
2024-09-08 14:05:34 +0300

    [Removed global interrupt enabler]

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -7,6 +7,16 @@ extern no_interrupt_handler
 global int21h
 global idt_load
 global no_interrupt
+global enable_interrupts
+global disable_interrupts
+
+enable_interrupts:
+  sti
+  ret
+
+disable_interrupts:
+  cli
+  ret
 
 idt_load:
     push ebp
@@ -33,3 +43,4 @@ no_interrupt:
     popad
     sti
     iret
+
